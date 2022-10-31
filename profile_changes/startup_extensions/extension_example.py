d227068c660d49b4d8f680dfd2fb0022d4d9b9b6
Mario Kostelac <mario.kostelac@gmail.com>
2022-10-31 13:10:27 +0100

    Add more instructions

--- profile_changes/startup_extensions/extension_example.py
+++ profile_changes/startup_extensions/extension_example.py
@@ -2,8 +2,8 @@
 Example of a simple extension that will reliably run on ipykernel startup.
 and fail loudly if there's some error.
 """
-
-# You can put security guardrails startup code here.
+# import numpy as np
+# You can put your code or security guardrails startup code here.
 
 def load_ipython_extension(ipython):
   # If you need a reference to ipython API, put the code here.
@@ -11,6 +11,6 @@ def load_ipython_extension(ipython):
   pass
 
   # If you need to make some variables available to the user namespace, you can do it like following
-  #
-  # import numpy as np
   # ipython.user_ns['np'] = np
+
+  # If you have no code here, you can delete the method.
